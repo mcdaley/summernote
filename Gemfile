cf88a003f4038105affa0157d12a858691ed0762
Mike Daley <michaelcdaley@yahoo.com>
2016-11-04 12:56:16 -0700

    Added the summernote editor for creating and editing tasks

--- Gemfile
+++ Gemfile
@@ -1,27 +1,28 @@
 source  'https://rubygems.org'
 ruby    '2.2.2'
 
-gem     'rails',            '4.2.4'
+gem     'rails',              '4.2.4'
 gem     'sqlite3'
-gem     'sass-rails',       '~> 5.0'
-gem     'uglifier',         '>= 1.3.0'
-gem     'coffee-rails',     '~> 4.1.0'
+gem     'sass-rails',         '~> 5.0'
+gem     'uglifier',           '>= 1.3.0'
+gem     'coffee-rails',       '~> 4.1.0'
 gem     'jquery-rails'
 gem     'jquery-ui-rails'
 gem     'turbolinks'
-gem     'jbuilder',         '~> 2.0'
+gem     'jbuilder',           '~> 2.0'
 gem     'bootstrap-sass'
 gem     'devise'
 gem     'haml-rails'
 gem     'high_voltage'
 gem     'simple_form'
+gem     'summernote-rails',   '~> 0.8'
 
 group :development do
   gem   'better_errors'
   gem   'html2haml'
-  gem   'hub',              :require=>nil
+  gem   'hub',                :require=>nil
   gem   'rails_layout'
-  gem   'web-console',      '~> 2.0'
+  gem   'web-console',        '~> 2.0'
   gem   'spring'
 end
 
--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,23 +2,63 @@
 // app/assets/javascripts/tasks.js
 //-----------------------------------------------------------------------------
 
+/** 
+ * Initialize the summernote editor for all of the tasks
+ */
+jQuery.fn.initTaskEditor = function() {
+  $(this).each(function(){
+    $(this).summernote({
+      toolbar:  false
+    });
+  });
+  
+  return this;
+}
+
+/**
+ * Initialize the summernote editor for notes, which includes the toolbar
+ */
+jQuery.fn.initNoteEditor = function() {
+  $(this).each(function(){
+    $(this).summernote({
+      toolbar: [
+          // [groupName, [list of button]]
+          ['style',     ['bold', 'italic', 'underline', 'clear']],
+          //** ['font',      ['strikethrough']],
+          ['fontsize',  ['fontsize']],
+          //** ['color',     ['color']],
+          ['para',      ['ul', 'ol', 'paragraph']],
+          ['link',      ['link']]
+        ],
+        placeholder: 'Enter description...'
+    });
+  });
+  
+  return this;
+}
+
 /**
  * Clear the task form after adding a new task
  */
 jQuery.fn.clearTaskForm = function() {
   console.log("[clearTaskForm]: Called method to clear the task form");
 
-  this.find( '#task_description'  ).val('').focus();
- 	this.find( '#task_due_text'     ).val('');
- 	this.find( '.form-group'        ).removeClass('has-error');
-
+  //** this.find( '#task_description'  ).val('').focus();
+  this.find( '[data-provider="summernote"]' ).summernote('reset');
+ 	this.find( '#task_due_text'               ).val('');
+ 	this.find( '.form-group'                  ).removeClass('has-error');
+  
   return this;
 }
 
 //-----------------------------------------------------------------------------
 // main()
 //-----------------------------------------------------------------------------
-$(function() {
+$(document).ready(function() {
   // Turn on the datepicker
   $( ".datepicker" ).datepicker();
+  
+  $('[data-provider="summernote"').initTaskEditor();
+  //** $('[data-provider="summernote"').initNoteEditor();
+  
 });
